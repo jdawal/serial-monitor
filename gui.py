768ec9f6f9bd9728363174d6dbc106c9ef5ed0bf
Joseph Dawal <jdawal@live.com>
2023-11-23 16:21:23 +0400

    Update gui.py

--- gui.py
+++ gui.py
@@ -20,11 +20,15 @@ def send_serial_message(ser, user_input, output_text):
         output_text.insert(tk.END, f"Sent message: {complete_message}\n")
 
 
-def monitor_and_respond_gui(port, baudrate=9600):
-    def on_send_button():
-        user_input = input_entry.get()
-        send_serial_message(ser, user_input, output_text)
+def monitor_serial_data(ser, output_text):
+    while True:
+        received_data = ser.readline().decode("utf-8").strip()
+        if "GNGLL" in received_data:
+            output_text.insert(tk.END, f"Received data: {received_data}\n")
+        output_text.update_idletasks()
 
+
+def monitor_and_respond_gui(port, baudrate=9600):
     try:
         ser = serial.Serial(port, baudrate, timeout=1)
 
@@ -39,11 +43,22 @@ def monitor_and_respond_gui(port, baudrate=9600):
         input_entry = ttk.Entry(window)
         input_entry.pack(pady=10)
 
-        send_button = ttk.Button(window, text="Send", command=on_send_button)
+        send_button = ttk.Button(window, text="Send", command=lambda: on_send_button())
         send_button.pack(pady=10)
 
         output_text = tk.Text(window, height=10, width=50)
         output_text.pack(pady=10)
+
+        # Start a separate thread to continuously monitor serial data
+        import threading
+
+        thread = threading.Thread(target=lambda: monitor_serial_data(ser, output_text))
+        thread.daemon = True
+        thread.start()
+
+        def on_send_button():
+            user_input = input_entry.get()
+            send_serial_message(ser, user_input, output_text)
 
         window.mainloop()
 
